a3e97b655c3ffac12eb03805666919aba8ad7aa6
Faisal-88 <faisaltimbangan32@gmail.com>
2023-11-10 15:04:40 +0700

    Menambahkan form input add with firebase_database diflutter

---
+++ lib/firebase_learn/add_food_firebase.dart
@@ -0,0 +1,102 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_week_4/firebase_learn/home_screen.dart';
+import 'package:flutter_week_4/firebase_learn/res_food.dart';
+
+class addFoodFirebase extends StatefulWidget {
+  final String? userId;
+  const addFoodFirebase({super.key, this.userId });
+
+  @override
+  State<addFoodFirebase> createState() => _addFoodFirebaseState();
+}
+
+class _addFoodFirebaseState extends State<addFoodFirebase> {
+
+      final FirebaseDatabase database = FirebaseDatabase.instance;
+      TextEditingController nama = TextEditingController();
+      TextEditingController asal = TextEditingController();
+      TextEditingController lat = TextEditingController();
+      TextEditingController lon = TextEditingController();
+
+      Future<void> addKuliner() async {
+        ResFood res = ResFood(nama.text, asal.text, lat.text, lon.text, widget.userId);
+        await database.ref().child("kuliner").child(res.key ?? "").push().set(res.toJson());
+      }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Center(
+        child: Text("Add Food")),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Nama Makanan"
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Asal Makanan"
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Latitude"
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              controller: lon,
+              decoration: const InputDecoration(
+                hintText: "Longitude"
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Center(
+              child: MaterialButton(
+                color: Colors.green,
+                height: 45,
+                minWidth: 200,
+                textColor: Colors.white,
+                onPressed: ()  async{
+                  if(nama.text.isEmpty || 
+                      asal.text.isEmpty || 
+                      lat.text.isEmpty || 
+                      lon.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("form wajib diisi semua"),
+                      backgroundColor: Colors.green,
+                      ),
+                    );
+                  } else {
+                    await addKuliner().then((value) {
+                      Navigator.pushAndRemoveUntil(context,
+                       MaterialPageRoute(builder: (_) => const HomePage()), 
+                       (route) => false);
+                    });
+                  }
+                },
+                child: const Text("SIMPAN"),
+              ),
+            )
+          ],
+        ),
+    );
+  }
+}
--- lib/firebase_learn/home_screen.dart
+++ lib/firebase_learn/home_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_week_4/firebase_learn/add_food_firebase.dart';
 import 'package:flutter_week_4/firebase_learn/authentication.dart';
+import 'package:flutter_week_4/firebase_learn/res_food.dart';
 
 class HomePage extends StatefulWidget {
   final String? userId;
@@ -13,15 +18,72 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseDatabase database = FirebaseDatabase.instance;
+  DatabaseReference? databaseReference;
+  List<ResFood> listFood = [];
+  Query? queryFood;
+  StreamSubscription<DatabaseEvent>? onAddList;
+
+  Future<void> getData() async {
+    queryFood = database.ref().child("kuliner").orderByChild("userId").equalTo(widget.userId);
+    onAddList = queryFood?.onChildAdded.listen(updateList); 
+  }
+
+  Future<void> updateList(DatabaseEvent event) async {
+    setState(() {
+      listFood.add(ResFood.fromSnapshoot(event.snapshot));
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement iniState
+    super.initState();
+    getData();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    onAddList?.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          "Hello world App",
-          style: TextStyle(color: Colors.white)),
+          "Food App",
+          style: TextStyle(color: Colors.white)
+          ),
         backgroundColor: Colors.green,
       ),
+      body: listFood.isEmpty
+            ? const Center(
+              child: Text("Belum ada data"),
+            )
+            : ListView.builder(
+            itemCount: listFood.length,
+            itemBuilder: (context, index) {
+              ResFood data = listFood[index];
+              return ListTile(
+                title: Text("${data.namaMakanan}"),
+              );
+            }),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          onPressed: () {
+            showDialog(context: context, builder: (context) {
+              return addFoodFirebase(
+                userId: widget.userId
+              );
+            });
+          },
+          child: const Icon(
+            Icons.add
+          ),
+        ),
     );
   }
 }
